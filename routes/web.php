a4a4f9c0787fdf3d6df90ef444c972553943c9e5
Muhammad Iqbal <assegaf.iqbal99@gmail.com>
2025-04-10 12:50:33 +0700

    feat: add temporary routes for migrate and storage link

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate --force');
+    return ' Migration Done!';
+});
+
+Route::get('/link-storage', function () {
+    Artisan::call('storage:link');
+    return ' Storage Linked!';
 });
